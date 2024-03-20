864a41e58ba9a89758b9164811f7065d07deabde
Manoj Puthran <putmanoj@in.ibm.com>
2024-03-20 06:37:27 -0700

    [draft] add spec for .create_in_provider

---
+++ spec/models/manageiq/providers/embedded_terraform/automation_manager/configuration_script_source_spec.rb
@@ -0,0 +1,61 @@
+RSpec.describe ManageIQ::Providers::EmbeddedTerraform::AutomationManager::ConfigurationScriptSource do
+  context "with a local repo" do
+    let(:manager) do
+      FactoryBot.create(:provider_embedded_terraform, :default_organization => 1).managers.first
+    end
+
+    let(:params) do
+      {
+        :name    => "hello_world",
+        :scm_url => "file://#{local_repo}"
+      }
+    end
+
+    let(:clone_dir)          { Dir.mktmpdir }
+    let(:local_repo)         { File.join(clone_dir, "hello_world_local") }
+    let(:repo_dir)           { Pathname.new(Dir.mktmpdir) }
+    let(:repos)              { Dir.glob(File.join(repo_dir, "*")) }
+    let(:repo_dir_structure) { %w[hello_world.tf] }
+
+    before do
+      puts("IN before()")
+      FileUtils.mkdir_p(local_repo)
+
+      repo = Spec::Support::FakeAnsibleRepo.new(local_repo, repo_dir_structure)
+      repo.generate
+      repo.git_branch_create("other_branch")
+
+      GitRepository
+      stub_const("GitRepository::GIT_REPO_DIRECTORY", repo_dir)
+
+      # # EvmSpecHelper.assign_embedded_terraform_role
+      puts("DONE before()")
+    end
+
+    # Clean up repo dir after each spec
+    after do
+      puts("IN after()")
+      FileUtils.rm_rf(repo_dir)
+      FileUtils.rm_rf(clone_dir)
+      puts("DONE after()")
+    end
+
+    describe ".create_in_provider" do
+      it "creates a record and initializes a git repo" do
+        puts("IN it()")
+        result = described_class.create_in_provider(manager.id, params)
+
+        expect(result).to(be_an(described_class))
+        # expect(result.scm_type).to eq("git")
+        # expect(result.scm_branch).to eq("master")
+        # expect(result.status).to eq("successful")
+        # expect(result.last_updated_on).to be_an(Time)
+        # expect(result.last_update_error).to be_nil
+
+        # git_repo_dir = repo_dir.join(result.git_repository.id.to_s)
+        # expect(files_in_repository(git_repo_dir)).to eq ["hello_world.tf"]
+        puts("DONE it()")
+      end
+    end
+  end
+end
