c5d1461c69f2510ca296abd5a781253261ef6a02
Manoj Puthran <putmanoj@in.ibm.com>
2024-03-20 23:38:21 -0700

    Add more tests

--- spec/models/manageiq/providers/embedded_terraform/automation_manager/configuration_script_source_spec.rb
+++ spec/models/manageiq/providers/embedded_terraform/automation_manager/configuration_script_source_spec.rb
@@ -57,4 +57,77 @@ RSpec.describe ManageIQ::Providers::EmbeddedTerraform::AutomationManager::Config
       end
     end
   end
+
+  describe "git_repository interaction" do
+    let(:auth) { FactoryBot.create(:embedded_terraform_scm_credential) }
+    let(:configuration_script_source) do
+      described_class.create!(
+        :name           => "foo",
+        :scm_url        => "https://example.com/foo.git",
+        :authentication => auth
+      )
+    end
+
+    it "on .create" do
+      configuration_script_source
+
+      git_repository = GitRepository.first
+      expect(git_repository.name).to eq "foo"
+      expect(git_repository.url).to eq "https://example.com/foo.git"
+      expect(git_repository.authentication).to eq auth
+
+      expect { configuration_script_source.git_repository }.to_not make_database_queries
+      expect(configuration_script_source.git_repository_id).to eq git_repository.id
+    end
+
+    it "on .new" do
+      configuration_script_source = described_class.new(
+        :name           => "foo",
+        :scm_url        => "https://example.com/foo.git",
+        :authentication => auth
+      )
+
+      expect(GitRepository.count).to eq 0
+
+      attached_git_repository = configuration_script_source.git_repository
+
+      git_repository = GitRepository.first
+      expect(git_repository).to eq attached_git_repository
+      expect(git_repository.name).to eq "foo"
+      expect(git_repository.url).to eq "https://example.com/foo.git"
+      expect(git_repository.authentication).to eq auth
+
+      expect { configuration_script_source.git_repository }.to_not make_database_queries
+      expect(configuration_script_source.git_repository_id).to eq git_repository.id
+    end
+
+    it "errors when scm_url is invalid" do
+      expect do
+        configuration_script_source.update!(:scm_url => "invalid url")
+      end.to raise_error(ActiveRecord::RecordInvalid)
+    end
+
+    it "syncs attributes down" do
+      configuration_script_source.name = "bar"
+      expect(configuration_script_source.git_repository.name).to eq "bar"
+
+      configuration_script_source.scm_url = "https://example.com/bar.git"
+      expect(configuration_script_source.git_repository.url).to eq "https://example.com/bar.git"
+
+      configuration_script_source.authentication = nil
+      expect(configuration_script_source.git_repository.authentication).to be_nil
+    end
+
+    it "persists attributes down" do
+      configuration_script_source.update!(:name => "bar")
+      expect(GitRepository.first.name).to eq "bar"
+
+      configuration_script_source.update!(:scm_url => "https://example.com/bar.git")
+      expect(GitRepository.first.url).to eq "https://example.com/bar.git"
+
+      configuration_script_source.update!(:authentication => nil)
+      expect(GitRepository.first.authentication).to be_nil
+    end
+
+  end
 end
