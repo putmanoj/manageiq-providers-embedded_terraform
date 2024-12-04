066a63c35485abd2ae4262fa68505549ad69d205
MANOJ PUTHRAN <putmanoj@in.ibm.com>
2024-12-04 13:16:04 +0530

    Add method to normalize ManageIQ dialog field values to terraform runner supported value
    For example
    * boolean 'T' to true
    * stringified json to json

--- lib/terraform/runner/api_params.rb
+++ lib/terraform/runner/api_params.rb
@@ -21,7 +21,7 @@ module Terraform
         param_list
       end
 
-      # convert to format required by terraform-runner api
+      # Convert to format required by terraform-runner api
       def self.to_cam_param(param_name, param_value, is_secured: false)
         {
           'name'    => param_name,
@@ -34,12 +34,67 @@ module Terraform
       #
       # @param vars [Hash] Hash with key/value pairs that will be passed as input variables to the
       #        terraform-runner run
+      #
       # @return [Array] Array of {:name,:value}
       def self.to_cam_parameters(vars)
         return [] if vars.nil?
 
         vars.map do |key, value|
           to_cam_param(key, value)
+        end
+      end
+
+      require 'set'
+      TRUE_VALUES = ['T', 't', true, 'true', 'True', 'TRUE'].to_set
+
+      # Normalize variables values, from ManageIQ values to Terraform Runner supported values
+      # @param input_vars       [Hash]  key/value pairs as input variables for the terraform-runner run job.
+      # @param type_constraints [Array] array of type constraints objects, from Terraform Runner
+      #
+      # @return [Array] Array of param objects [{:name,:value}]
+      def self.to_normalized_cam_parameters(input_vars, type_constraints)
+        require 'json'
+
+        input_vars.map do |k, v|
+          type_constr = type_constraints.find { |e| e[:name] == k.to_s }
+
+          secured = false
+          if !type_constr.nil?
+            secured = TRUE_VALUES.include?(type_constr[:secured])
+
+            case type_constr[:type]
+            when "boolean"
+              v = TRUE_VALUES.include?(v)
+
+            when "map"
+              if v.kind_of?(String)
+                begin
+                  v = JSON.parse(v)
+                rescue JSON::ParserError
+                  raise "The variable '#{k}' does not have valid hashmap value"
+                end
+              end
+
+              if !v.kind_of?(Hash)
+                raise "The variable '#{k}' does not have valid hashmap value"
+              end
+
+            when "list"
+              if v.kind_of?(String)
+                begin
+                  v = JSON.parse(v)
+                rescue JSON::ParserError
+                  raise "The variable '#{k}' does not have valid array value"
+                end
+              end
+
+              if !v.kind_of?(Array)
+                raise "The variable '#{k}' does not have valid array value"
+              end
+            end
+          end
+
+          to_cam_param(k, v, :is_secured => secured)
         end
       end
     end
