2f1d24f3b68072f4415f08cb73fd4ad223e89186
Adam Grare <adam@grare.com>
2026-05-14 14:42:15 -0400

    Add allowed_credential_types

--- app/models/manageiq/providers/embedded_terraform/automation_manager/provision_workflow.rb
+++ app/models/manageiq/providers/embedded_terraform/automation_manager/provision_workflow.rb
@@ -11,8 +11,25 @@ class ManageIQ::Providers::EmbeddedTerraform::AutomationManager::ProvisionWorkfl
   end
 
   def allowed_credentials(*_args)
-    ManageIQ::Providers::EmbeddedTerraform::AutomationManager::TemplateCredential.all.map do |auth|
-      build_ci_hash_struct(auth, %w[name type])
+    credential_type          = get_value(values[:credential_type])
+    allowed_credential_klass = credential_type&.safe_constantize || self.class.module_parent::TemplateCredential
+    allowed_credential_klass.all.to_h do |auth|
+      [auth.id, auth.name]
+    end
+  end
+
+  def allowed_credential_types(*_args)
+    credential_id = get_value(values[:credential_id])
+    credential    = self.class.module_parent::TemplateCredential.find_by(:id => credential_id) if credential_id
+
+    credential_classes = credential.present? ? [credential.class] : ManageIQ::Providers::EmbeddedTerraform::AutomationManager::TemplateCredential.descendants
+    credential_classes.to_h do |klass|
+      next unless klass.const_defined?(:API_OPTIONS)
+
+      api_options = klass::API_OPTIONS
+      next unless api_options[:label]
+
+      [klass.name, api_options[:label]]
     end
   end
 
--- spec/models/manageiq/providers/embedded_terraform/automation_manager/provision_workflow_spec.rb
+++ spec/models/manageiq/providers/embedded_terraform/automation_manager/provision_workflow_spec.rb
@@ -23,4 +23,159 @@ describe ManageIQ::Providers::EmbeddedTerraform::AutomationManager::ProvisionWor
       end
     end
   end
+
+  describe "#allowed_credentials" do
+    context "with no credentials" do
+      it "returns an empty hash" do
+        workflow = described_class.new({:provision_dialog_name => dialog.name}, admin.userid)
+        expect(workflow.allowed_credentials).to eq({})
+      end
+    end
+
+    context "with multiple credentials and no credential_type filter" do
+      let!(:amazon_cred)  { FactoryBot.create(:embedded_terraform_amazon_credential, :name => "AWS Prod") }
+      let!(:azure_cred)   { FactoryBot.create(:embedded_terraform_azure_credential, :name => "Azure Dev") }
+      let!(:vsphere_cred) { FactoryBot.create(:embedded_terraform_vsphere_credential, :name => "vSphere Test") }
+
+      it "returns all TemplateCredential instances" do
+        workflow = described_class.new({:provision_dialog_name => dialog.name}, admin.userid)
+        allowed = workflow.allowed_credentials
+
+        expect(allowed).to be_a(Hash)
+        expect(allowed.keys).to contain_exactly(amazon_cred.id, azure_cred.id, vsphere_cred.id)
+        expect(allowed[amazon_cred.id]).to eq("AWS Prod")
+        expect(allowed[azure_cred.id]).to eq("Azure Dev")
+        expect(allowed[vsphere_cred.id]).to eq("vSphere Test")
+      end
+    end
+
+    context "with credential_type filter for AmazonCredential" do
+      let!(:amazon_cred)  { FactoryBot.create(:embedded_terraform_amazon_credential, :name => "AWS Prod") }
+      let!(:azure_cred)   { FactoryBot.create(:embedded_terraform_azure_credential, :name => "Azure Dev") }
+      let!(:vsphere_cred) { FactoryBot.create(:embedded_terraform_vsphere_credential, :name => "vSphere Test") }
+
+      it "returns only Amazon credentials" do
+        workflow = described_class.new({:provision_dialog_name => dialog.name}, admin.userid)
+        workflow.values[:credential_type] = ["ManageIQ::Providers::EmbeddedTerraform::AutomationManager::AmazonCredential", nil]
+        allowed = workflow.allowed_credentials
+
+        expect(allowed).to be_a(Hash)
+        expect(allowed.keys).to contain_exactly(amazon_cred.id)
+        expect(allowed[amazon_cred.id]).to eq("AWS Prod")
+      end
+    end
+
+    context "with credential_type filter for AzureCredential" do
+      let!(:amazon_cred)  { FactoryBot.create(:embedded_terraform_amazon_credential, :name => "AWS Prod") }
+      let!(:azure_cred)   { FactoryBot.create(:embedded_terraform_azure_credential, :name => "Azure Dev") }
+      let!(:vsphere_cred) { FactoryBot.create(:embedded_terraform_vsphere_credential, :name => "vSphere Test") }
+
+      it "returns only Azure credentials" do
+        workflow = described_class.new({:provision_dialog_name => dialog.name}, admin.userid)
+        workflow.values[:credential_type] = ["ManageIQ::Providers::EmbeddedTerraform::AutomationManager::AzureCredential", nil]
+        allowed = workflow.allowed_credentials
+
+        expect(allowed).to be_a(Hash)
+        expect(allowed.keys).to contain_exactly(azure_cred.id)
+        expect(allowed[azure_cred.id]).to eq("Azure Dev")
+      end
+    end
+
+    context "with invalid credential_type" do
+      let!(:amazon_cred)  { FactoryBot.create(:embedded_terraform_amazon_credential, :name => "AWS Prod") }
+      let!(:azure_cred)   { FactoryBot.create(:embedded_terraform_azure_credential, :name => "Azure Dev") }
+
+      it "falls back to all TemplateCredentials" do
+        workflow = described_class.new({:provision_dialog_name => dialog.name}, admin.userid)
+        workflow.values[:credential_type] = ["InvalidClassName", nil]
+        allowed = workflow.allowed_credentials
+
+        expect(allowed).to be_a(Hash)
+        expect(allowed.keys).to contain_exactly(amazon_cred.id, azure_cred.id)
+      end
+    end
+  end
+
+  describe "#allowed_credential_types" do
+    context "with no credential_id specified" do
+      it "returns all TemplateCredential descendant types with API_OPTIONS" do
+        workflow = described_class.new({:provision_dialog_name => dialog.name}, admin.userid)
+        allowed = workflow.allowed_credential_types
+
+        expect(allowed).to be_a(Hash)
+        expect(allowed.keys).to include(
+          "ManageIQ::Providers::EmbeddedTerraform::AutomationManager::AmazonCredential",
+          "ManageIQ::Providers::EmbeddedTerraform::AutomationManager::AzureCredential",
+          "ManageIQ::Providers::EmbeddedTerraform::AutomationManager::VsphereCredential",
+          "ManageIQ::Providers::EmbeddedTerraform::AutomationManager::GoogleCredential",
+          "ManageIQ::Providers::EmbeddedTerraform::AutomationManager::OpenstackCredential",
+          "ManageIQ::Providers::EmbeddedTerraform::AutomationManager::IbmCloudCredential",
+          "ManageIQ::Providers::EmbeddedTerraform::AutomationManager::IbmClassicInfrastructureCredential"
+        )
+      end
+
+      it "returns correct labels for each credential type" do
+        workflow = described_class.new({:provision_dialog_name => dialog.name}, admin.userid)
+        allowed = workflow.allowed_credential_types
+
+        expect(allowed["ManageIQ::Providers::EmbeddedTerraform::AutomationManager::AmazonCredential"]).to eq("Amazon")
+        expect(allowed["ManageIQ::Providers::EmbeddedTerraform::AutomationManager::AzureCredential"]).to eq("Azure")
+      end
+    end
+
+    context "with credential_id for AmazonCredential" do
+      let!(:amazon_cred) { FactoryBot.create(:embedded_terraform_amazon_credential, :name => "AWS Prod") }
+
+      it "returns only the AmazonCredential type" do
+        workflow = described_class.new({:provision_dialog_name => dialog.name}, admin.userid)
+        workflow.values[:credential_id] = [amazon_cred.id, nil]
+        allowed = workflow.allowed_credential_types
+
+        expect(allowed).to be_a(Hash)
+        expect(allowed.keys).to contain_exactly("ManageIQ::Providers::EmbeddedTerraform::AutomationManager::AmazonCredential")
+        expect(allowed["ManageIQ::Providers::EmbeddedTerraform::AutomationManager::AmazonCredential"]).to eq("Amazon")
+      end
+    end
+
+    context "with credential_id for AzureCredential" do
+      let!(:azure_cred) { FactoryBot.create(:embedded_terraform_azure_credential, :name => "Azure Dev") }
+
+      it "returns only the AzureCredential type" do
+        workflow = described_class.new({:provision_dialog_name => dialog.name}, admin.userid)
+        workflow.values[:credential_id] = [azure_cred.id, nil]
+        allowed = workflow.allowed_credential_types
+
+        expect(allowed).to be_a(Hash)
+        expect(allowed.keys).to contain_exactly("ManageIQ::Providers::EmbeddedTerraform::AutomationManager::AzureCredential")
+        expect(allowed["ManageIQ::Providers::EmbeddedTerraform::AutomationManager::AzureCredential"]).to eq("Azure")
+      end
+    end
+
+    context "with credential_id for VsphereCredential" do
+      let!(:vsphere_cred) { FactoryBot.create(:embedded_terraform_vsphere_credential, :name => "vSphere Test") }
+
+      it "returns only the VsphereCredential type" do
+        workflow = described_class.new({:provision_dialog_name => dialog.name}, admin.userid)
+        workflow.values[:credential_id] = [vsphere_cred.id, nil]
+        allowed = workflow.allowed_credential_types
+
+        expect(allowed).to be_a(Hash)
+        expect(allowed.keys).to contain_exactly("ManageIQ::Providers::EmbeddedTerraform::AutomationManager::VsphereCredential")
+      end
+    end
+
+    context "with invalid credential_id" do
+      it "falls back to all TemplateCredential descendants" do
+        workflow = described_class.new({:provision_dialog_name => dialog.name}, admin.userid)
+        workflow.values[:credential_id] = [99_999, nil]
+        allowed = workflow.allowed_credential_types
+
+        expect(allowed).to be_a(Hash)
+        expect(allowed.keys).to include(
+          "ManageIQ::Providers::EmbeddedTerraform::AutomationManager::AmazonCredential",
+          "ManageIQ::Providers::EmbeddedTerraform::AutomationManager::AzureCredential"
+        )
+      end
+    end
+  end
 end
