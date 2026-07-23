49588cfd5f08221e35ae5a3782b1106d778788cb
Adam Grare <adam@grare.com>
2026-07-23 12:33:19 -0400

    Check Terraform::Runner.available? in Retire StateMachine

--- app/models/manageiq/providers/embedded_terraform/automation_manager/retire/state_machine.rb
+++ app/models/manageiq/providers/embedded_terraform/automation_manager/retire/state_machine.rb
@@ -1,3 +1,16 @@
 module ManageIQ::Providers::EmbeddedTerraform::AutomationManager::Retire::StateMachine
   extend ActiveSupport::Concern
+
+  # If the Terraform::Runner isn't available then don't start the retirement process
+  def run_retire
+    return requeue_phase unless Terraform::Runner.available?
+
+    signal :start_retirement
+  end
+
+  def remove_from_provider
+    super
+  rescue Terraform::Runner::TemporarilyUnavailable
+    requeue_phase
+  end
 end
