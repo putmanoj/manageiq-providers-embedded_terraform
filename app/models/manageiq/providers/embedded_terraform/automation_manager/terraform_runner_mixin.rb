c4dbe4c2e82a93b05cf7056218249345e52a2bf2
Adam Grare <adam@grare.com>
2026-09-08 14:52:51 -0400

    Mark terraform runner error message for translation

--- app/models/manageiq/providers/embedded_terraform/automation_manager/terraform_runner_mixin.rb
+++ app/models/manageiq/providers/embedded_terraform/automation_manager/terraform_runner_mixin.rb
@@ -13,7 +13,7 @@ module ManageIQ::Providers::EmbeddedTerraform::AutomationManager::TerraformRunne
     unavailable_duration = Time.now.utc - phase_context[:terraform_runner_unavailable_since]
 
     if unavailable_duration > TERRAFORM_RUNNER_UNAVAILABLE_TIMEOUT
-      error_message = "Terraform Runner has been unavailable for #{unavailable_duration.to_i} seconds, exceeding timeout of #{TERRAFORM_RUNNER_UNAVAILABLE_TIMEOUT.to_i} seconds"
+      error_message = _("Terraform Runner has been unavailable for %{unavailable} seconds, exceeding timeout of %{timeout} seconds") % {:unavailable => unavailable_duration.to_i, :timeout => TERRAFORM_RUNNER_UNAVAILABLE_TIMEOUT.to_i}
       _log.error(error_message)
       raise MiqException::MiqProvisionError, error_message
     end
